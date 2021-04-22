94890f05aaff2d2abafa27cc69d3c8ecf103365f
Gasbasman <baskoppenaal@hotmail.com>
2021-04-22 22:03:50 +0200

    putting everything online

---
+++ My_own_RMarkdown_File_yo.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: "My own RMarkdown file yo"
+#' author: "Bas Koppenaal"
+#' date: "18-4-2021"
+#' output: html_document
+#' ---
+#' 
+#' This is my first RMarkdown file tracked by Git!!
+#' 
+#' look at this shit, again
+#' 
+#' look at me
+#' 
+#' 
+## ----setup, include=FALSE---------------------------------------------------------------------------------------------
+
